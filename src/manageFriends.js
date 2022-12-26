07f3828dae36cbd2b0b7e757d8aa0ca1eba170cb
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-12-26 15:52:43 +0300

    Done

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,13 @@
-export function manageFriends(state, action) {
+const initialState = {friends: []}
+export function manageFriends(state = initialState, action) {
   // your code here
+  switch(action.type){
+    case("friends/add"):
+    return {...state, friends: [...state.friends, action.payload]}
+    case("friends/remove"):
+    const removeFriends = [...state.friends]
+    return {...state, friends: removeFriends.splice(action.payload + 1, 1)}
+    default:
+      return state
+  }
 }
